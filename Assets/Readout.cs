ff179debc5e154440bfff5995f81f61210a6915f
Bonozo <bonozogames@gmail.com>
2017-04-03 20:36:56 -0500

    update title muzak by M_O_J_O_E, readout for story

---
+++ Assets/Readout.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Readout : MonoBehaviour {
+
+    public Text TextObj;
+    public float charDelay = 0.0f;
+    public string readoutText;
+
+    private string readoutParsed;
+    private int readoutIdx = 0;
+    private float elapsed = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        readoutParsed = readoutText.Replace("^", "\n");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        elapsed += Time.fixedDeltaTime;
+
+        int endPos = (int)(elapsed / charDelay);
+        if (endPos > readoutParsed.Length)
+            endPos = readoutParsed.Length;
+
+        TextObj.text = readoutParsed.Substring(0, endPos);
+    }
+}
